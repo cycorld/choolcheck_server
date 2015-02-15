dd53cf9d308cb027e6f7e1c7a974380168496970
cycorld <choi@likelion.net>
2015-02-16 04:47:32 +0900

    가입할때 추가정보 받기

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def index
 
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
   private
   def load_classes
     @classes = Klass.all
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :dept
+    devise_parameter_sanitizer.for(:sign_up) << :code
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :dept
+    devise_parameter_sanitizer.for(:account_update) << :code
   end
 end
